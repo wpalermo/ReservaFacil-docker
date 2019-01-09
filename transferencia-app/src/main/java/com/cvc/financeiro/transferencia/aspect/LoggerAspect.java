f30f261ec6c70552f22ae250b474fbe4093d0ff4
William Moraes <williammoraes@cvccorp.com.br>
2019-01-09 11:31:59 -0200

    docker com imagem na cloud e incio do kub

--- transferencia-app/src/main/java/com/cvc/financeiro/transferencia/aspect/LoggerAspect.java
+++ transferencia-app/src/main/java/com/cvc/financeiro/transferencia/aspect/LoggerAspect.java
@@ -7,6 +7,7 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StopWatch;
 
 @Aspect
 @Configuration
@@ -15,7 +16,7 @@ public class LoggerAspect {
 
     Logger logger = LogManager.getLogger(this.getClass());
 
-    @Around("execution(* *(..)) && @annotation(LogThis)")
+   /* @Around("execution(* *(..)) && @annotation(LogThis)")
     public Object around(ProceedingJoinPoint point)
 		    throws Throwable {
 
@@ -30,6 +31,39 @@ public class LoggerAspect {
 		    System.currentTimeMillis() - start);
 
         return result;
+    }*/
+
+
+    @Around("execution(* com.cvc.financeiro.transferencia..*.*(..))")
+    public Object logTimeMethod(ProceedingJoinPoint joinPoint) throws Throwable {
+
+	StopWatch stopWatch = new StopWatch();
+	stopWatch.start();
+
+	Object retVal = joinPoint.proceed();
+
+	stopWatch.stop();
+
+	StringBuffer logMessage = new StringBuffer();
+	logMessage.append(joinPoint.getTarget().getClass().getName());
+	logMessage.append(".");
+	logMessage.append(joinPoint.getSignature().getName());
+	logMessage.append("(");
+	// append args
+	Object[] args = joinPoint.getArgs();
+	for (int i = 0; i < args.length; i++) {
+	    logMessage.append(args[i]).append(",");
+	}
+	if (args.length > 0) {
+	    logMessage.deleteCharAt(logMessage.length() - 1);
+	}
+
+	logMessage.append(")");
+	logMessage.append(" execution time: ");
+	logMessage.append(stopWatch.getTotalTimeMillis());
+	logMessage.append(" ms");
+	logger.info(logMessage.toString());
+	return retVal;
     }
 
 }
